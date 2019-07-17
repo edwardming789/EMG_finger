db847869aef1432f413e07e15e0af8a6abdc7957
Yiming Yao <yiming.yao@mail.mcgill.ca>
2019-07-17 18:24:09 -0400

    change

--- OhmMeter/OhmMeter.ino
+++ OhmMeter/OhmMeter.ino
@@ -2,8 +2,8 @@ int analogPin= 0;
 int raw= 0;
 int Vin= 5;
 float Vout= 0;
-float F = 0;
-float R1= 1000;
+int F = 0;
+float R1= 100000;
 float R2= 0;
 float buffer= 0;
 
@@ -15,21 +15,15 @@ Serial.begin(9600);
 void loop()
 {
 raw= analogRead(analogPin);
-if(raw) 
-{
-buffer= raw * Vin;
-Vout= (buffer)/1024.0;
-buffer= (Vin/Vout) -1;
-R2= R1 * buffer/1000.0;
-if (R2 < 25) {
-  F = 100000000.0 * (pow(R2,-5.08));
-}else{
-  F = 0;
-}
-
-//Serial.print("Vout: ");
-//Serial.println(Vout);
-//Serial.print("R2: ");
-Serial.println(R2);
-}
+  if(raw) 
+  {
+  buffer= raw * Vin;
+  Vout= (buffer)/1024.0;
+  buffer= (Vin/Vout) -1;
+  R2= R1 * buffer/1000.0;
+    if ((R2 <=120) && (R2>=80)){
+      F = map(R2,120,80,100,400);
+    }
+  Serial.println(F);
+  }
 }
